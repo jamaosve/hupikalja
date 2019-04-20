14a69c09a927518e6242f1b99d1b2c244c017b8b
Jammi Vesterinen <jammi@Jammis-MacBook-Pro.local>
2019-04-20 17:37:43 +0300

    testi

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to edit :D
+            Kuka siin gitis on??? >:(
           </p>
           <a
             className="App-link"
